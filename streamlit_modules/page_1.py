06edc3e0283439a8c23566f1df587af42e4c5994
Datageek2525 <63169589+taesanbeck@users.noreply.github.com>
2023-06-15 01:17:04 -0400

    page_1 updated

--- streamlit_modules/page_1.py
+++ streamlit_modules/page_1.py
@@ -1,8 +1,33 @@
 # page_1.py
 import streamlit as st
+from PIL import Image
 
-def show_page(button, slider, selectbox):
-    st.write("This is page 1")
-    # Use the button, slider, and selectbox values as needed
+def show_page(slider, selectbox):
+    st.title('Model Page')
 
+    st.header('Upload an image:')
+    uploaded_file = st.file_uploader("Choose an image file", type=["jpg", "jpeg", "png"])
 
+    if uploaded_file is not None:
+        image = Image.open(uploaded_file)
+        st.image(image, caption='Uploaded Image', width=500)
+
+        st.header('Confidence Level:')
+        confidence_level = st.slider('Adjust the confidence level', min_value=0.0, max_value=1.0, value=0.5)
+
+        st.header('Bounding Boxes:')
+        bounding_box_option = st.radio('Would you like bounding boxes displayed?', ('Yes', 'No'))
+
+        # Note: Replace "model_prediction" with model's prediction function
+        # cv_labels = our_model.predict(image, confidence_level, bounding_box_option)
+
+        # Display the labels
+        # st.header('Computer Vision Labels:')
+        # st.text(cv_labels)
+
+        # Pass the labels to your NLP model for description
+        # nlp_description = your_nlp_model.describe(cv_labels)
+
+        # Display the description
+        # st.header('NLP Description:')
+        # st.text(nlp_description)
