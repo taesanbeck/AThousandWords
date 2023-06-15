4e951387056171214183568630765b084c6c3f54
Datageek2525 <63169589+taesanbeck@users.noreply.github.com>
2023-06-15 01:18:57 -0400

    sidbar code updated

--- streamlit_modules/side_bar.py
+++ streamlit_modules/side_bar.py
@@ -1,24 +1,21 @@
 # sidebar.py
 import streamlit as st
+from PIL import Image
+
 
 def show_sidebar(page_options):
-    # Add a title to the sidebar
-    st.sidebar.title("Thousand Words")
-
+    st.sidebar.image('streamlit_modules/images/gmu_image.png', output_format='auto',channels='RGB')
     # Add navigation
     page = st.sidebar.selectbox("Go to", page_options, key="sidebar_selectbox")
 
     return page
 
-def show_sidebar_1():
-    st.sidebar.title("Thousand Words - Page 1")
-    # Add a button
-    button = st.sidebar.button('Button 1')
+def show_sidebar_1():   
     # Add a slider
-    slider = st.sidebar.slider('Model Slider', min_value=0, max_value=100, value=50)
+    slider = st.sidebar.slider('Model Confidence Level Slider', min_value=0, max_value=100, value=50)
     # Add a selectbox
-    selectbox = st.sidebar.selectbox('Select Me', ['Option 1', 'Option 2', 'Option 3'])
-    return button, slider, selectbox
+    selectbox = st.sidebar.selectbox('Select Model', ['Yolo', 'DINO', 'DINOv2'])
+    return slider, selectbox
 
 def show_sidebar_2():
     st.sidebar.title("Thousand Words - Page 2")
@@ -29,7 +26,7 @@ def show_sidebar_2():
     return slider, selectbox
 
 def show_sidebar_3():
-    st.sidebar.title("Thousand Words - Page 3")
+    st.sidebar.title("About")
     # Add a button
     button = st.sidebar.button('Button 1')
     return button
