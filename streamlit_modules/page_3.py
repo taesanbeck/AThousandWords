b4d7f5925f82cba6d8299446edba122d8b02748d
Datageek2525 <63169589+taesanbeck@users.noreply.github.com>
2023-06-22 02:55:55 -0400

    m

--- streamlit_modules/page_3.py
+++ streamlit_modules/page_3.py
@@ -1,10 +1,9 @@
 import streamlit as st
-from PIL import Image
+
 
 def show_page(selectbox):
     # Team photo
-    image = Image.open("streamlit_modules/page_3.png")
-    st.image(image)
+    st.image("streamlit_modules/page_3.png")
     
     # Formatting Center Align
     st.markdown(
